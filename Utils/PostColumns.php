568953e74450458a7ebc3b70252d4e965012da3b
sharjeel <leo8545@gmail.com>
2020-08-31 14:22:41 +0500

    added comments to utils methods

--- Utils/PostColumns.php
+++ Utils/PostColumns.php
@@ -2,6 +2,11 @@
 
 class PostColumns
 {
+	/**
+	 * Post type name
+	 *
+	 * @var string
+	 */
 	private $postType;
 
 	public function __construct(string $postType)
@@ -9,11 +14,23 @@ class PostColumns
 		$this->postType = $postType;
 	}
 
+	/**
+	 * Adds filter to posts columns of the post type
+	 *
+	 * @param mixed $callable
+	 * @return void
+	 */
 	public function add($callable)
 	{
 		add_filter("manage_{$this->postType}_posts_columns", $callable, 10, 1);
 	}
 
+	/**
+	 * Adds action to custom column
+	 *
+	 * @param mixed $callable
+	 * @return void
+	 */
 	public function populate($callable)
 	{
 		add_action("manage_{$this->postType}_posts_custom_column", $callable, 10, 2);
